77c420500b102ca8cf9785f52ca9abc8bcb81f5e
Dan Griggs <dgriggs@fideliscare.org>
2015-12-11 20:17:18 -0500

    7

---
+++ AdventOfCode/AdventOfCode/DaySeven.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace AdventOfCode
+{
+    class DaySeven
+    {
+        static List<string> input = new List<string>() {  };
+
+        public static void Run()
+        {
+            PartOne();
+            PartTwo();
+            
+        }
+
+        static void PartOne()
+        {
+
+            int answer = 0;
+            
+
+            Console.WriteLine($"Day 7 Part 1 Answer : {answer}");
+
+        }
+        
+        static void PartTwo()
+        {
+
+            int answer = 0;
+            
+            Console.WriteLine($"Day 7 Part 2 Answer : {answer}");
+        }
+
+       
+
+    }
+
+}
